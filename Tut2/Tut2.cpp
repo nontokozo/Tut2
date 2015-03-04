3fb715072ea6b8fde972c90a7add7fafedd6a1af
nontokozo <nontokozondlovu96@gmail.com>
2015-03-04 16:09:40 +0200

    adding fuctions

--- Tut2/Tut2.cpp
+++ Tut2/Tut2.cpp
@@ -2,6 +2,46 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+using namespace std;
+class Fraction
+{
+public:
+	Fraction(int num, int den);  // default constructor
+
+	void setFraction(int a, int b);
+	int getFraction(); // get values from the user
+
+
+	// function declaraction
+
+	int add();
+	int subtract();
+	int multiply();
+	int divide();
+	int print();
+
+	~Fraction();
+
+private:
+	int numerator;
+	int denominator;
+
+	
+};
+
+Fraction::Fraction(int num =1, int den=1)
+{
+	numerator = num;
+	denominator = den;
+}
+
+Fraction::~Fraction()
+{
+}
+
+
+
 
 
 int _tmain(int argc, _TCHAR* argv[])
